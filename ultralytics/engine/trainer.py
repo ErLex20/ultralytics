cc638ac14748b63c9b04bba1c0cf04008d380b39
ErLex20 <alexandru.cretu@students.uniroma2.eu>
2026-07-31 20:13:05 +0200

    feat: epoch param

--- ultralytics/engine/trainer.py
+++ ultralytics/engine/trainer.py
@@ -865,6 +865,7 @@ class BaseTrainer:
                 self.args = get_cfg(ckpt_args)
                 self.args.model = self.args.resume = str(last)  # reinstate model
                 for k in (
+                    "epochs",
                     "imgsz",
                     "batch",
                     "device",
